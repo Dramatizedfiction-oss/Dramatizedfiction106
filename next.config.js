23e27fcf3cbbc2866ea1cb0cfa8eb639644c0d34
Dramatizedfiction-oss <benthailand2023@gmail.com>
2026-04-09 12:55:37 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**"
-      }
-    ]
-  }
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
+
