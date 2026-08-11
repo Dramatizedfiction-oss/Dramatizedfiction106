08ee6bd735cea2d78250196b9337670e5f823880
Dramatizedfiction-oss <benthailand2023@gmail.com>
2026-08-11 06:13:48 -0400

    fix(writer-studio): accept redesignEnabled prop in WriterStudioShell and pass to Sidebar

--- components/writer/WriterStudioShell.tsx
+++ components/writer/WriterStudioShell.tsx
@@ -4,13 +4,17 @@ import WriterStudioSidebar from "@/components/writer-studio/Sidebar";
 export default function WriterStudioShell({
   user,
   children,
+  redesignEnabled = false,
 }: {
   user: AuthUser | null;
   children: React.ReactNode;
+  redesignEnabled?: boolean;
 }) {
   return (
     <div className="px-4 py-6 md:px-6 lg:px-8">
-      <WriterStudioSidebar user={user}>{children}</WriterStudioSidebar>
+      <WriterStudioSidebar user={user} redesignEnabled={redesignEnabled}>
+        {children}
+      </WriterStudioSidebar>
     </div>
   );
 }
